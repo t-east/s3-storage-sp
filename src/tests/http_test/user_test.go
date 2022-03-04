7b0e233c874af6e5a9eadaf25f0c55a99b7875cc
t-east <azmtomya.a127@gmail.com>
2022-03-04 19:21:55 +0900

    http test

--- src/tests/http_test/user_test.go
+++ src/tests/http_test/user_test.go
@@ -5,69 +5,48 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
-	"testing"
+	"sp/src/asserts"
 	"sp/src/domains/entities"
 	"sp/src/interfaces/contracts"
 	"sp/src/interfaces/controllers"
-	"sp/src/mocks"
-	"sp/src/usecases/interactor"
+	"testing"
 
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
-func SetupUserTestController() (*controllers.UserController, error) {
+func LoadTestDB() (*gorm.DB, error) {
 	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 	if err != nil {
 		return nil, err
 	}
 	db.AutoMigrate(&entities.User{})
-	uc := &controllers.UserController{
-		OutputFactory: mocks.NewUserOutputPortMock(),
-		InputFactory:  interactor.NewUserInputPor,
-		RepoFactory:   mocks.NewUserRepository,
-		Param:         contracts.Param,
-		Conn:          db,
-	}
-	return uc, nil
+	return db, nil
+}
+
+func LoadTestParam() (*contracts.Param, error) {
+	return &contracts.Param{}, nil
 }
 
 // UserName, EmailのあるユーザをPOST -> 201を返すかをテスト
 func TestCreateUser(t *testing.T) {
-	uc, err := SetupUserTestController()
+	db, err := LoadTestDB()
 	if err != nil {
 		t.Errorf("Failed to get DB: %v", err)
 		return
 	}
 	var buf bytes.Buffer
-	if err := json.NewEncoder(&buf).Encode(&handler.RequestUser{
-		UserName: "test",
-		Email:    "test@test.com",
+	if err := json.NewEncoder(&buf).Encode(&entities.User{
+		ID:      "1",
+		Address: "sdf",
+		PubKey:  "pubKey",
+		PrivKey: "privKey",
 	}); err != nil {
 		t.Fatal(err)
 	}
 	req := httptest.NewRequest(http.MethodPost, "/api/users", &buf)
 	rec := httptest.NewRecorder()
-	uc.Post(rec, req)
+	uc := controllers.LoadUserController(db)
+	uc.Dispatch(rec, req)
 	asserts.AssertEqual(t, http.StatusCreated, rec.Code, rec.Result().Status)
-}
-
-// UserNameが無いユーザをPOST -> 400を返すかをテスト
-func TestCreateUserError(t *testing.T) {
-	uc, err := SetupUserTestController()
-	if err != nil {
-		t.Errorf("Failed to get DB: %v", err)
-		return
-	}
-	var buf bytes.Buffer
-	if err := json.NewEncoder(&buf).Encode(&handler.RequestUser{
-		UserName: "",
-		Email:    "test@test.com",
-	}); err != nil {
-		t.Fatal(err)
-	}
-	req := httptest.NewRequest(http.MethodPost, "/api/users/", &buf)
-	rec := httptest.NewRecorder()
-	uc.Post(rec, req)
-	asserts.AssertEqual(t, http.StatusBadRequest, rec.Code, rec.Result().Status)
 }
