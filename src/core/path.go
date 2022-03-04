f1465b1544b353ca4bb2ce820b7bfcdbdbdf0c64
t-east <azmtomya.a127@gmail.com>
2022-03-05 08:14:20 +0900

    ULID作成

--- src/core/path.go
+++ src/core/path.go
@@ -1,8 +1,12 @@
 package core
 
 import (
+	"math/rand"
 	"path"
 	"strings"
+	"time"
+
+	"github.com/oklog/ulid"
 )
 
 func ShiftPath(p string) (head string, tail string) {
@@ -12,4 +16,10 @@ func ShiftPath(p string) (head string, tail string) {
 		return p[1:], "/"
 	}
 	return p[1:i], p[i:]
+}
+
+func MakeULID() string {
+	t := time.Now()
+	entropy := ulid.Monotonic(rand.New(rand.NewSource(t.UnixNano())), 0)
+	return ulid.MustNew(ulid.Timestamp(t), entropy).String()
 }
