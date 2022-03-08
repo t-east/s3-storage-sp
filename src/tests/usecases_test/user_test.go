6279e707195d14b8938e0ed884ec715caa99e613
t-east <azmtomya.a127@gmail.com>
2022-03-09 06:03:06 +0900

    エラー判定にassertsを用いる

--- src/tests/usecases_test/user_test.go
+++ src/tests/usecases_test/user_test.go
@@ -1,35 +1,26 @@
 package usecases_test
 
 import (
-	"testing"
+	"sp/src/asserts"
 	"sp/src/domains/entities"
 	"sp/src/mocks"
 	"sp/src/usecases/interactor"
+	"testing"
 )
 
 func TestUserCreate(t *testing.T) {
 	outputPort := mocks.NewUserOutputPortMock()
 	repository := mocks.NewUserRepositoryMock()
 	inputPort := interactor.NewUserInputPort(outputPort, repository)
-
-	user := &entities.User{Address: "sdf", PubKey: "sdf", PrivKey: "sdf"}
+	user := &entities.User{Address: "sdf", PubKey: []byte("sdf"), PrivKey: []byte("sdf")}
 
 	created, err := inputPort.Create(user)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if created.ID != "7" {
-		t.Errorf("User.Create() should return entities.User.ID = 7, but got = %s", created.ID)
-	}
-	if created.Address != user.Address {
-		t.Errorf("User.Create() should return entities.User.Address = %s, but got = %s", user.Address, created.Address)
-	}
-	if created.PubKey != user.PubKey {
-		t.Errorf("User.Create() should return entities.User.PubKey = %s, but got = %s", user.PubKey, created.PubKey)
-	}
-	if created.PrivKey != user.PrivKey {
-		t.Errorf("User.Create() should return entities.User.PrivKey = %s, but got = %s", user.PrivKey, created.PrivKey)
-	}
+	asserts.AssertEqual(t, created.Address, user.Address, "アドレスが一致していません")
+	asserts.AssertEqual(t, created.PubKey, user.PubKey, "公開鍵が一致していません")
+	asserts.AssertEqual(t, created.PubKey, user.PubKey, "公開鍵が一致していません")
 }
 
 func TestUserFindByID(t *testing.T) {
@@ -37,23 +28,15 @@ func TestUserFindByID(t *testing.T) {
 	repository := mocks.NewUserRepositoryMock()
 	inputPort := interactor.NewUserInputPort(outputPort, repository)
 
-	user := &entities.User{Address: "sdf", PubKey: "sdf", PrivKey: "sdf"}
+	user := &entities.User{Address: "sdf", PubKey: []byte("sdf"), PrivKey: []byte("sdf")}
 
 	id := "7"
 	found, err := inputPort.FindByID(id)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if found.ID != id {
-		t.Errorf("User.Create() should return entities.User.ID = 7, but got = %s", found.ID)
-	}
-	if found.Address != user.Address {
-		t.Errorf("User.Create() should return entities.User.Address = %s, but got = %s", user.Address, found.Address)
-	}
-	if found.PubKey != user.PubKey {
-		t.Errorf("User.Create() should return entities.User.PubKey = %s, but got = %s", user.PubKey, found.PubKey)
-	}
-	if found.PrivKey != user.PrivKey {
-		t.Errorf("User.Create() should return entities.User.PrivKey = %s, but got = %s", user.PrivKey, found.PrivKey)
-	}
+	asserts.AssertEqual(t, found.ID, id, "IDが一致していません")
+	asserts.AssertEqual(t, found.Address, user.Address, "アドレスが一致していません")
+	asserts.AssertEqual(t, found.PubKey, user.PubKey, "公開鍵が一致していません")
+	asserts.AssertEqual(t, found.PubKey, user.PubKey, "公開鍵が一致していません")
 }
